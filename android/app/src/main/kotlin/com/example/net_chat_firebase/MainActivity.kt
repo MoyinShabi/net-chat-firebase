8cb25faac2e1dcd1a6cd03341f5554269bcfb024
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-10-29 03:47:04 +0100

    first commit
    * configured firebase
    *added custom font & image assets

---
+++ android/app/src/main/kotlin/com/example/net_chat_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.net_chat_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'pages/auth_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const NetChat());
+}
+
+final theme = ThemeData(
+  fontFamily: 'Satoshi',
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  appBarTheme: const AppBarTheme(
+    centerTitle: true,
+    elevation: 0,
+    scrolledUnderElevation: 0,
+  ),
+);
+
+class NetChat extends StatelessWidget {
+  const NetChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NetChat',
+      theme: theme,
+      home: const AuthPage(),
+    );
+  }
+}
